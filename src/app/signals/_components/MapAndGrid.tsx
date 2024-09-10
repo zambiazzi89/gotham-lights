963504c59a12719910f879373f7569d9056016c6
zambiazzi89 <zambiazzi89@gmail.com>
2024-09-09 22:42:08 -0400

    Move buttons out of MapAndGrid

--- src/app/signals/_components/MapAndGrid.tsx
+++ src/app/signals/_components/MapAndGrid.tsx
@@ -5,9 +5,6 @@ import { LatLong, Signal } from '@/lib/types'
 import { useEffect, useState } from 'react'
 import ReactGoogleMap from '@/components/ReactGoogleMap'
 import GoogleAutocompleteInput from '@/components/googleAutocomplete'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import MyLocationButton from '../_components/MyLocationButton'
 import { useRouter } from 'next/navigation'
 
 export default function MapAndGrid({ signals }: { signals: Signal[] }) {
@@ -33,33 +30,20 @@ export default function MapAndGrid({ signals }: { signals: Signal[] }) {
   }, [boundsNE, boundsSW])
 
   return (
-    <>
-      <div className="h-full flex flex-col overflow-y-auto lg:grid lg:overflow-hidden lg:grid-cols-2">
-        <div className="lg:h-full m-4">
-          <div className="m-4">
-            <GoogleAutocompleteInput />
-          </div>
-          <div className="h-[400px] md:h-[500px] lg:h-[85%]">
-            <ReactGoogleMap
-              signals={signals}
-              setBoundsNE={setBoundsNE}
-              setBoundsSW={setBoundsSW}
-            />
-          </div>
+    <div className="h-full flex flex-col overflow-y-auto lg:grid lg:overflow-hidden lg:grid-cols-2">
+      <div className="lg:h-full m-4">
+        <div className="m-4">
+          <GoogleAutocompleteInput />
         </div>
-        <SignalCardGrid signalsInBound={signalsInBound} />
-      </div>
-      <div className="h-14 px-4 flex justify-between items-center gap-4">
-        <div className="flex gap-4">
-          <Link href="/signals?viewAll=true">
-            <Button variant={'secondary'}>View All</Button>
-          </Link>
-          <MyLocationButton />
+        <div className="h-[400px] md:h-[500px] lg:h-[85%]">
+          <ReactGoogleMap
+            signals={signals}
+            setBoundsNE={setBoundsNE}
+            setBoundsSW={setBoundsSW}
+          />
         </div>
-        <Link href="/signals/create" className="h-8">
-          <Button className="px-8 sm:px-16 font-bold">Create a Signal</Button>
-        </Link>
       </div>
-    </>
+      <SignalCardGrid signalsInBound={signalsInBound} />
+    </div>
   )
 }
--- src/app/signals/page.tsx
+++ src/app/signals/page.tsx
@@ -2,6 +2,9 @@ import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import MapAndGrid from './_components/MapAndGrid'
 import db from '@/db/db'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import MyLocationButton from './_components/MyLocationButton'
 
 export default async function Signals() {
   const signals = await db.signal.findMany({})
@@ -10,6 +13,17 @@ export default async function Signals() {
     <div className="h-svh grid grid-rows-layout-signals">
       <Navbar />
       <MapAndGrid signals={signals} />
+      <div className="h-14 px-4 flex justify-between items-center gap-4">
+        <div className="flex gap-4">
+          <Link href="/signals?viewAll=true">
+            <Button variant={'secondary'}>View All</Button>
+          </Link>
+          <MyLocationButton />
+        </div>
+        <Link href="/signals/create" className="h-8">
+          <Button className="px-8 sm:px-16 font-bold">Create a Signal</Button>
+        </Link>
+      </div>
       <Footer />
     </div>
   )
