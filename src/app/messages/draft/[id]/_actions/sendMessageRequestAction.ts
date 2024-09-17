da1f77fe147346b0ea33df3a460576db99ef18e2
zambiazzi89 <zambiazzi89@gmail.com>
2024-09-17 17:51:30 -0400

    Implemented Send Message Request Action

---
+++ src/app/messages/draft/[id]/_actions/sendMessageRequestAction.ts
@@ -0,0 +1,80 @@
+'use server'
+
+import db from '@/db/db'
+import getDbProfileFromServer from '@/utils/supabase/customFunctions/getDbProfileFromServer'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(4, {
+      message: 'Username length must be between 4 and 12 characters.',
+    })
+    .max(12, {
+      message: 'Username length must be between 4 and 12 characters.',
+    }),
+  content: z
+    .string()
+    .min(1, {
+      message:
+        'Your message content length must be between 1 and 1000 characters.',
+    })
+    .max(1000, {
+      message:
+        'Your message content length must be between 1 and 10000 characters.',
+    }),
+})
+
+export async function sendMessageRequest(
+  prevState: unknown,
+  formData: FormData
+) {
+  // Only perform the action if user is logged in
+  const profile = await getDbProfileFromServer()
+
+  if (!profile.username) {
+    console.error('No username found for profile')
+    redirect('/profile')
+  }
+
+  // Continue if logged in and has username
+
+  const result = formSchema.safeParse(Object.fromEntries(formData.entries()))
+
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors
+  }
+
+  const data = result.data
+
+  const conversation = await db.conversation.create({
+    data: {
+      status: 'Pending',
+    },
+  })
+
+  await db.conversation_participant.create({
+    data: {
+      conversation_id: conversation.id,
+      participant_username: profile.username,
+    },
+  })
+
+  await db.conversation_participant.create({
+    data: {
+      conversation_id: conversation.id,
+      participant_username: data.username,
+    },
+  })
+
+  await db.message.create({
+    data: {
+      conversation_id: conversation.id,
+      from_username: profile.username,
+      content: data.content,
+    },
+  })
+
+  redirect('/messages')
+}
--- src/app/messages/draft/[id]/_components/MessageRequestForm.tsx
+++ src/app/messages/draft/[id]/_components/MessageRequestForm.tsx
@@ -1,26 +1,37 @@
+'use client'
+
 import { CancelButtonWithDialog } from '@/components/AlertDialog'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import SubmitButton from '@/components/SubmitButton'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Textarea } from '@/components/ui/textarea'
+import { useFormState } from 'react-dom'
+import { sendMessageRequest } from '../_actions/sendMessageRequestAction'
+import { Input } from '@/components/ui/input'
 
 export default function MessageRequestForm({
   toUsername,
 }: {
   toUsername: string
 }) {
+  const [error, action] = useFormState(sendMessageRequest, {})
   return (
     <div className="grid place-items-center">
-      <Card className="w-[80svw] p-4">
+      <Card className="w-[80svw] p-4 bg-secondary">
         <CardHeader>
-          <div>To {toUsername}</div>
+          <div className="font-bold text-lg self-center">
+            Send a message request to {toUsername}
+          </div>
         </CardHeader>
         <CardContent>
-          <div>Message</div>
-          <form>
-            <Textarea className="h-60" />
+          <form action={action}>
+            <Input type="hidden" name="username" value={toUsername} />
+            <Textarea name="content" className="h-60" />
+            {error?.content && (
+              <div className="text-destructive text-sm">{error.content}</div>
+            )}
             <div className="py-4 flex justify-end">
               <CancelButtonWithDialog />
-              <Button>Send</Button>
+              <SubmitButton />
             </div>
           </form>
         </CardContent>
