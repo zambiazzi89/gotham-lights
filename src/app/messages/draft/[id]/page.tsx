12fbc9ff61baef2bea6e0a05451a59a6e0db5d40
zambiazzi89 <zambiazzi89@gmail.com>
2024-10-02 20:49:49 -0400

    Redirect if Conversation already exists

--- src/app/messages/draft/[id]/page.tsx
+++ src/app/messages/draft/[id]/page.tsx
@@ -2,6 +2,8 @@ import getDbProfileFromServer from '@/utils/supabase/customFunctions/getDbProfil
 import MessageRequestForm from './_components/MessageRequestForm'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import db from '@/db/db'
+import { redirect } from 'next/navigation'
 
 export default async function MessageDraft({
   params: id,
@@ -10,6 +12,43 @@ export default async function MessageDraft({
 }) {
   const { id: toUsername } = id
   const profile = await getDbProfileFromServer()
+
+  if (profile.username) {
+    const conversationExists = await db.conversation.findMany({
+      where: {
+        AND: [
+          {
+            conversation_participants: {
+              some: {
+                participant_username: profile.username,
+              },
+            },
+          },
+          {
+            conversation_participants: {
+              some: {
+                participant_username: toUsername,
+              },
+            },
+          },
+          {
+            conversation_participants: {
+              every: {
+                participant_username: { in: [profile.username, toUsername] },
+              },
+            },
+          },
+        ],
+      },
+      select: {
+        id: true,
+      },
+    })
+    if (conversationExists.length > 0) {
+      redirect(`/messages/${conversationExists[0].id}`)
+    }
+  }
+
   return (
     <>
       {profile.username ? (
