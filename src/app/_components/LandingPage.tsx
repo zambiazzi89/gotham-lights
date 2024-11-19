705eb23545733e30c69bef37ed2b4fa1a7494b51
zambiazzi89 <zambiazzi89@gmail.com>
2024-11-19 14:56:31 -0500

    Fix dependency issues

--- src/app/_components/LandingPage.tsx
+++ src/app/_components/LandingPage.tsx
@@ -1,6 +1,14 @@
 import GoogleAutocompleteInput from '@/components/GoogleTools/googleAutocomplete'
+import { Button } from '@/components/ui/button'
+import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const supabase = createClient()
+  const { data: sessionData } = await supabase.auth.getSession()
+
+  const session = sessionData.session
+
   return (
     <div className="flex flex-col justify-center items-center text-white">
       <div className="max-w-[80vw] flex flex-col items-center bg-black bg-opacity-50  backdrop-blur-sm p-8 rounded border-white">
@@ -12,7 +20,13 @@ export default function LandingPage() {
             Send them a signal!
           </h2>
         </div>
-        <GoogleAutocompleteInput />
+        {!!session ? (
+          <GoogleAutocompleteInput />
+        ) : (
+          <Link href="/login">
+            <Button className="text-black">Login</Button>
+          </Link>
+        )}
       </div>
     </div>
   )
