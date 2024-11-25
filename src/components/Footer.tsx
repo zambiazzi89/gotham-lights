5d37e0cbd6edfca1b84693a3891a17a067eb450c
zambiazzi89 <zambiazzi89@gmail.com>
2024-11-25 11:34:36 -0500

    Improve Footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div className="p-2 text-stone-400 text-xs bg-black backdrop-blur cursor-default flex justify-between">
-      <div className="flex gap-2">
-        <Link href="/contact">Contact</Link>
+    <div className="p-2 text-stone-400 text-xs bg-black backdrop-blur cursor-default flex justify-between gap-2">
+      <div className="flex flex-col min-[390px]:flex-row min-[390px]:gap-2">
         <Link href="/terms-and-conditions">Terms and Conditions</Link>
         <Link href="/privacy-policy">Privacy Policy</Link>
       </div>
-      <div>Copyright © 2024</div>
+      <div className="flex flex-col min-[390px]:flex-row min-[390px]:gap-2 items-end">
+        <Link href="/contact">Contact</Link>
+        <div>Copyright © 2024</div>
+      </div>
     </div>
   )
 }
